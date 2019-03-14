388665cc1c0d5f5d0ee7b0e6d4ab26ecff06941a
Daniel Goldberg <danielgoldberg@Daniels-MBP.fios-router.home>
2019-03-14 17:13:45 -0400

    Fill out empty user specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,36 +3,59 @@ require 'rails_helper'
 RSpec.describe User, :type => :model do
   
   let(:user) {
-    User.create(
+    User.new(
       name: "John Doe",
       email: "johndoe2000@gmail.com",
-      password_digest: "password",
+      password: "password",
+      password_confirmation: "password",
       birthdate: "2000-12-31"
     )
   }
 
-  it "is valid with a name, email, and password_digest" do
+  it 'has all necessary fields' do
+    expect(User.new).to respond_to(:name)
+    expect(User.new).to respond_to(:email)
+    expect(User.new).to respond_to(:birthdate)
+    expect(User.new).to respond_to(:password)
+    expect(User.new).to respond_to(:password_confirmation)
+  end
+
+  it "is valid with a name, email, password, and matching password_confirmation" do
     expect(user).to be_valid
   end
 
-  it "is invalid without a name" do
-
+  it "is valid if password is set and password_confirmation is nil" do
+     user.password_confirmation = nil
+     expect(user.valid?).to be true 
   end
 
-  it "is invalid without a password_digest" do 
+  it "is invalid if password and password_confirmation are both non-nil and don't match" do
+    user.password_confirmation = 'passw0rd'
+    expect(user.valid?).to be false
+  end
 
+  it "is invalid without a name" do
+    user.name = nil
+    expect(user).not_to be_valid
+  end
+
+  it "is invalid with a password fewer than 6 characters" do 
+    user.password = "short"
+    user.password_confirmation = nil
+    expect(user).not_to be_valid
+    user.password_confirmation = "short"
+    expect(user).not_to be_valid
   end
 
   it "is invalid without an email" do
-
+    user.email = nil
+    expect(user).not_to be_valid
   end
 
   it "is invalid with an e-mail that has already been taken" do
-
-  end
-
-  it "is invalid with a password fewer than 6 characters" do
-
+    user_two = user
+    user.save
+    expect(user_two).not_to be_valid
   end
 
 end
