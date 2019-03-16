29277dd904377f4d192eb899d4bf277d68c31049
Daniel Goldberg <danielgoldberg@Daniels-MBP.fios-router.home>
2019-03-15 22:07:23 -0400

    Add transaction method to capitalize direction before validating

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -8,4 +8,12 @@ class Transaction < ApplicationRecord
 	validates :quantity, numericality: { greater_than_or_equal_to: 1}
 	validates :amount, numericality: { greater_than: 0}
 
+	before_validation :capitalize_direction
+
+	private
+
+	def capitalize_direction
+		direction.capitalize!
+	end
+
 end
