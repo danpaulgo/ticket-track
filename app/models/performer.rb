fa5a252b944c1612ce32ae25f01b6f4a38133988
Daniel Goldberg <danielgoldberg@Daniels-MBP.fios-router.home>
2019-03-16 03:30:01 -0400

    Add performer methods to titleize name and validate uniqueness

--- app/models/performer.rb
+++ app/models/performer.rb
@@ -4,5 +4,12 @@ class Performer < ApplicationRecord
 	has_many :venues, through: :events
 
 	validates :name, presence: true
+	validates :name, uniqueness: true
+
+	before_validation :titleize_name
+
+	def titleize_name
+		self.name = name.titleize if name
+	end
 
 end
