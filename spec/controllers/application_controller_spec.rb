3fa6030547eef28ef4b782509ae3fae6c651f8e6
Daniel Goldberg <danielgoldberg@Daniels-MBP.fios-router.home>
2019-03-16 03:02:53 -0400

    Add application controller tests and routes

---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationController, type: :controller do
+	
+	include_context "fixtures"
+
+	describe "GET #home" do
+    it "returns a success response for logged out user" do
+      get :home, session: {user_id: nil}
+      expect(response).to be_successful
+    end
+
+    it "redirects to user show page for logged in user" do
+    	get :home, session: {user_id: user.id}
+    	response.should redirect_to user
+    end
+  end
+end
