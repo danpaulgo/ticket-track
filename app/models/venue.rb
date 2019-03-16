f70bb072e930ec97f67f02f5067f296cf355bdbc
Daniel Goldberg <danielgoldberg@Daniels-MBP.fios-router.home>
2019-03-15 23:06:42 -0400

    Add venue validations

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -3,4 +3,7 @@ class Venue < ApplicationRecord
 	has_many :events
 	has_many :performers, through: :events
 
+	validates :name, :city, :state, presence: true
+	validates :name, uniqueness: { scope: [:city, :state] }
+
 end
