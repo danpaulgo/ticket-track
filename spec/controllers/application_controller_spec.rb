336e4e45621aa07337bd92af904dab846c97cbc7
Daniel Goldberg <danielgoldberg@Daniels-MBP.fios-router.home>
2019-03-18 00:44:11 -0400

    Refactor current_user method and tests

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -15,4 +15,20 @@ RSpec.describe ApplicationController, type: :controller do
     	expect(response).to redirect_to user
     end
   end
+
+  describe "current_user" do
+    context "with no user logged in" do
+      it "returns nil" do
+        get :home, session: {user_id: nil}
+        expect(controller.current_user).to be_nil
+      end
+    end
+
+    context "with user logged in" do
+      it "returns user matching the sessions user_id" do
+        get :home, session: {user_id: user.id}
+        expect(controller.current_user).to eq(user)
+      end
+    end
+  end
 end
