861ebc325243a6b129e9def584664c1e1e8556df
Daniel Goldberg <danielgoldberg@Daniels-MBP.fios-router.home>
2019-04-05 18:10:28 -0400

    Add tests to performers controller spec

--- spec/controllers/performers_controller_spec.rb
+++ spec/controllers/performers_controller_spec.rb
@@ -61,26 +61,28 @@ RSpec.describe PerformersController, type: :controller do
   	end
   end
 
-  describe "POST #update" do
-  	context "admin with valid attributes" do
-  		before(:each) do 
-  			post :update, params: {id: performer.id, performer: valid_attributes}, session: admin_session
-  		end
+  describe "PATCH #update" do
+  	context "admin" do
+  		context "with valid attributes" do
+	  		before(:each) do 
+	  			patch :update, params: {id: performer.id, performer: valid_attributes}, session: admin_session
+	  		end
 
-  		it "updates performer" do
-	      expect(performer.name).to eq("Drizzy Drake")
-	    end
+	  		it "updates performer" do
+		      expect(performer.name).to eq("Drizzy Drake")
+		    end
 
-	    it "returns success response" do
-	    	expect(response).to be_successful
-	    end
-	  end
+		    it "returns success response" do
+		    	expect(response).to be_successful
+		    end
+	  	end
 
-	  context "admin with invalid attributes" do
-	    it "renders edit page" do
-	    	post :update, params: {id: performer.id, performer: invalid_attributes}, session: admin_session
-	      expect(response).to be_successful
-	    end
+		  context "with invalid attributes" do
+		    it "renders edit page" do
+		    	post :update, params: {id: performer.id, performer: invalid_attributes}, session: admin_session
+		      expect(response).to be_successful
+		    end
+	  	end
   	end
 
   	context "non admin" do
@@ -90,6 +92,57 @@ RSpec.describe PerformersController, type: :controller do
 	      get :edit, params: {id: performer.id}, session: logged_out_session
 	      expect(response).to redirect_to(root_path)
 	    end
+  	end
+  end
+
+  describe "DELETE #destroy" do
+  	context "admin" do
+  		context "with valid performer id"
+	  		before(:each) do 
+	  			delete :destroy, params: {id: performer.id}, session: admin_session
+	  		end
+	  		it "successfully deletes performer" do
+	  			expect(Performer.all).not_to include(performer)
+	  		end
+	  		it "redirects to perfomers index" do
+	  			expect(response).to redirect_to(perfomers_path)
+	  		end
+	  	end
+
+	  	context "with invalid performer id" do
+	  		it "does not delete any performers" do
+	  			expect {
+	          delete :destroy, params: {id: 99}, session: admin_session
+	        }.not_to change(User, :count)
+	  		end
+	  		it "redirects to perfomers index" do
+	  			delete :destroy, params: {id: 99}, session: admin_session
+	  			expect(response).to redirect_to(perfomers_path)
+	  		end
+	  	end
+
+  	context "logged in non admin" do
+  		it "does not delete performer" do
+  			expect {
+          delete :destroy, params: {id: performer.id}, session: logged_in_session
+        }.not_to change(User, :count)
+  		end
+  		it "redirects to perfomers index" do
+  			delete :destroy, params: {id: performer.id}, session: logged_in_session
+  			expect(response).to redirect_to(perfomers_path)
+	  	end
+  	end
+
+  	context "logged out user" do
+  		it "does not delete performer" do
+  			expect {
+          delete :destroy, params: {id: performer.id}, session: logged_out_session
+        }.not_to change(User, :count)
+  		end
+  		it "redirects to root path" do
+  			delete :destroy, params: {id: performer.id}, session: logged_out_session
+  			expect(response).to redirect_to(root_path)
+  		end
   	end
   end
 
