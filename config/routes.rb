8d7f6388d62b8338425d28fbcde4950bced1ed74
Daniel Goldberg <danielgoldberg@Daniels-MBP.fios-router.home>
2019-04-07 02:25:08 -0400

    Add venue resource routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :performers, except: [:new]
+  resources :venues
 
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
