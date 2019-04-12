fef2488654913311c63f5f254b5492b180e856e3
Daniel Goldberg <danielgoldberg@Daniels-MBP.fios-router.home>
2019-04-12 00:52:28 -0400

    Generate table, model, and spec for transaction_sources

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_12_043941) do
+ActiveRecord::Schema.define(version: 2019_04_12_044904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,12 @@ ActiveRecord::Schema.define(version: 2019_04_12_043941) do
   end
 
   create_table "performers", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "transaction_sources", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
