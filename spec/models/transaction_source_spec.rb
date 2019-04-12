aecbc0eab9ffa2a138b0e3665c70ae928b2585e2
Daniel Goldberg <danielgoldberg@Daniels-MBP.fios-router.home>
2019-04-12 00:56:52 -0400

    Add tests to transaction_source model spec

--- spec/models/transaction_source_spec.rb
+++ spec/models/transaction_source_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe TransactionSource, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  include_context "fixtures"
+
+	it 'has all necessary fields' do
+    expect(TransactionSource.new).to respond_to(:name)
+  end
+
+  it "is valid with a name" do
+    expect(ticketmaster).to be_valid
+  end
+
+  it "is invalid without a name" do
+  	ticketmaster.name = nil
+  	expect(ticketmaster).not_to be_valid
+  	stubhub.name = ""
+  	expect(stubhub).not_to be_valid
+  end
+
+  it "deletes all associated events transactions upon being deleted" do
+    ticketmaster.destroy
+    expect(Event.all).not_to include(purchase)
+  end
+
+
 end
