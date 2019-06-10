efc1e84d0f92c0eed64a957f10064d9577147593
Daniel Goldberg <danielgoldberg@Daniels-MBP.fios-router.home>
2019-06-10 02:44:56 -0400

    Refactor env configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
     authentication: :plain,
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
-    domain: 'heroku.com',
+    domain: 'agile-woodland-53497.herokuapp.com',
     enable_starttls_auto: true
   }
 
