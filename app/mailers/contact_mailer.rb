1120e2545575195617330308172229eb358aef68
Daniel Goldberg <danielgoldberg@Daniels-MBP.fios-router.home>
2019-06-15 02:26:27 -0400

    Refactor contact mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,7 +2,7 @@ class ContactMailer < ApplicationMailer
 
   def contact_message(contact)
     @contact = contact
-    mail to: "danpaulgo@aol.com", subject: contact.subject
+    mail to: "danpaulgo@aol.com", from: contact.email, subject: contact.subject
   end
 
 end
