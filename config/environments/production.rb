b344b801d9f2ce9eab3984b2ea079792beca53bf
Daniel Goldberg <danielgoldberg@Daniels-MBP.fios-router.home>
2019-07-28 03:03:56 -0400

    Update sendgrid host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'agile-woodland-53497.herokuapp.com'
+  host = 'ticket-track-heroku.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     address: 'smtp.sendgrid.net',
